af5651c529065386dd22d4b9e252de33b232196f
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-17 11:19:14 +0300

    add representation

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -9,6 +9,14 @@ def cover(f, data):
     return f(data[0], **data[1])
 '''
 
+def py_repr(f, data):
+    number = data["input"][0]
+    keywords = data["input"][1]
+    res = "{}({}".format(f, number)
+    for k, v in keywords.items():
+        res += ', {}={}'.format(k, v if not isinstance(v, str) else '"' + v + '"')
+    return res + ")"
+
 
 class Referee(RefereeBase):
     TESTS = TESTS
@@ -18,5 +26,10 @@ class Referee(RefereeBase):
     ENV_COVERCODE = {
         "python_2": cover_unwrap_with_keywords,
         "python_3": cover_unwrap_with_keywords,
+        "javascript": None
+    }
+    CALLED_REPRESENTATIONS = {
+        "python_2": py_repr,
+        "python_3": py_repr,
         "javascript": None
     }
